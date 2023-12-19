a30602a05d7d92d7c5cf2b65bcab00d909790211
Monika <okonecnikovaite.monika@gmail.com>
2023-12-20 01:01:47 +0200

    kill me

---
+++ 5/index5.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('#form');
+    const table = document.querySelector('#table');
+    const insertButton = document.querySelector('#insert');
+    const editButton = document.querySelector('#edit');
+    const deleteButton = document.querySelector('#del')
+
+
+    const tikrintiLS = localStorage.getItem("cartList");
+    const cartList = tikrintiLS === null ? [] : JSON.parse(tikrintiLS)
+
+
+
+insertButton.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    const id = document.querySelector("#id").value
+    const name = document.querySelector("#name").value
+    const quantity = document.querySelector("#quantity").value
+
+
+///tikrinti 
+if (!id || !name || !quantity) {
+                console.log("Blogai ivesti duomenys");
+                return;
+            }
+// const tikrintiLS = localStorage.getItem("cartList");
+// const cartList = tikrintiLS === null ? [] : JSON.parse(tikrintiLS)
+
+//prideti i LS
+const naujasProduktas = {
+    id,
+    name,
+    quantity
+}
+cartList.push(naujasProduktas);
+localStorage.setItem("cartList", JSON.stringify(cartList))
+
+});
+
+//edit'as
+editButton.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    const id = document.querySelector("#id").value
+    const name = document.querySelector("#name").value
+    const quantity = document.querySelector("#quantity").value
+
+    const updated = cartList.map(product =>{
+        if (product.id ===id){
+            return {id,name,quantity}
+        }
+        return product
+    });
+    localStorage.setItem('cartList', JSON.stringify(updated))
+})
+
+
+deleteButton.addEventListener('click', (event)=>{
+event.preventDefault();
+
+
+const id = document.querySelector('#id').value;
+
+localStorage.setItem("cartList", JSON.stringify(cartList))
+})
+
+
+
+
---
+++ 6new/index6.js
@@ -0,0 +1,14 @@
+   
+//     function updateTable(products) {
+//         // Clear the table
+//         table.innerHTML = '<th>Product Code</th><th>Product Name</th><th>Product quantity</th>';
+
+//         // Populate the table with the updated products
+//         products.forEach(product => {
+//             const row = table.insertRow();
+//             row.insertCell(0).innerText = product.id;
+//             row.insertCell(1).innerText = product.name;
+//             row.insertCell(2).innerText = product.quantity;
+//         });
+//     }
+// });
